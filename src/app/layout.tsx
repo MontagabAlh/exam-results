e36c4c446741a469b02793413d649463cf7c4713
Montagab AL-Hamawy <mntagab@qubefyn.com>
2025-04-05 11:20:24 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from 'next/script';
 const cairoFont = Cairo({
   variable: "--font-cairo",
   subsets: ["latin"],
@@ -33,6 +34,18 @@ export default function RootLayout({
         <div className="w-full">
         <Footer />
         </div>
+        <Script id="custom-stack-analytix" strategy="afterInteractive">
+           {
+             `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5362007,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+           `}
+            </Script>
       </body>
     </html>
   );
